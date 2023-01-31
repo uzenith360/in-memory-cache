048ec01846f19a5e68ccc91c7a1a23e9e79a15f2
Zenith Wogwugwu <uzenith360@users.noreply.github.com>
2023-01-31 23:05:38 +0100

    Delete lib directory

--- lib/cjs/types/in-memory-cache-item.interface.d.ts
+++
@@ -1,5 +0,0 @@
-export default interface InMemoryCacheItem<T> {
-    value: T;
-    expiry?: number;
-}
-//# sourceMappingURL=in-memory-cache-item.interface.d.ts.map
--- lib/esm/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import InMemoryCache from "./in-memory-cache";
-import InMemoryCacheItem from "./in-memory-cache-item.interface";
-export default InMemoryCache;
-export type { InMemoryCacheItem };
-//# sourceMappingURL=index.d.ts.map
